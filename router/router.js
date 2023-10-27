bac40501b0e780eda7cf5b9dd17c21a2992f7aa9
wackydawg <juliannwadinobi098@gmail.com>
2023-10-27 23:04:50 +0100

    Added endpoint to get all books

--- router/router.js
+++ router/router.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const router = express.Router(); 
-const {
-   validateBookBody,
-   validateBookUpdateBody,
- } = require("../middleware/validation");
-const {getBook, createBook, updateBook, deleteBook} = require('../controllers/bookController');
+//const {
+ //  validateBookBody,
+   //validateBookUpdateBody,
+ //} = require("../middleware/validation");
+const {allBooks, getBook, createBook, updateBook, deleteBook} = require('../controllers/bookController');
 
 
 router.get("/api", (req, res) => {
    res.send('Woo-Hoo ths is my Book CRUD API!🙋')
 })
 
+router.get("/api/books", allBooks);
+
 router.get("/api/:bookID", getBook);
 
-router.post("/api", validateBookBody, createBook);
+router.post("/api",  createBook);
 
-router.put("/api/:bookID", validateBookBody, updateBook);
+router.put("/api/:bookID",  updateBook);
 
 router.delete("/api/:bookID", deleteBook);
 
