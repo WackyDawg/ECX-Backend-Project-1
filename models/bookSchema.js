180b656bfa50c96700f1e34fe2d4824516f77b09
wackydawg <juliannwadinobi098@gmail.com>
2023-10-27 23:03:10 +0100

    modified:   models/bookSchema.js

--- models/bookSchema.js
+++ models/bookSchema.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: true,
+        
     },
 
 }, { collection: "books" }); 
