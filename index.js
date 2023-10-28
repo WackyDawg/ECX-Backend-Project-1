833443fe26ebf57ef039670382930f7757dfde25
wackydawg <juliannwadinobi098@gmail.com>
2023-10-28 02:53:49 +0100

    fixed Mongoose warning message

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => {
     console.log("⚡ Successfully Connected To MongoDB 🚀");
